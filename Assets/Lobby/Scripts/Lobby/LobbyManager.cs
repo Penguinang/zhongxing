75f0f4080947829400852c41e6c6d9175e24be59
Penguinang <1157703273@qq.com>
2017-10-22 10:31:30 +0800

    删除了一些没用代码

--- Assets/Lobby/Scripts/Lobby/LobbyManager.cs
+++ Assets/Lobby/Scripts/Lobby/LobbyManager.cs
@@ -11,7 +11,6 @@ namespace Prototype.NetworkLobby
 {
     public class LobbyManager : NetworkLobbyManager 
     {
-//        static short MsgKicked = MsgType.Highest + 1;
 
         static public LobbyManager s_Singleton;
 
@@ -46,188 +45,16 @@ namespace Prototype.NetworkLobby
         {
             s_Singleton = this;
             _lobbyHooks = GetComponent<Prototype.NetworkLobby.LobbyHook>();
-//            currentPanel = mainMenuPanel;
-
-//            backButton.gameObject.SetActive(false);
             GetComponent<Canvas>().enabled = true;
 
             DontDestroyOnLoad(gameObject);
-
-//            SetServerInfo("Offline", "None");
         }
 
         public override void OnLobbyClientSceneChanged(NetworkConnection conn)
         {
 			Debug.Log ("scene");
 			logPanel.gameObject.SetActive (false);
-//            if (SceneManager.GetSceneAt(0).name == lobbyScene)
-//            {
-//                if (topPanel.isInGame)
-//                {
-//                    ChangeTo(lobbyPanel);
-//                    if (_isMatchmaking)
-//                    {
-//                        if (conn.playerControllers[0].unetView.isServer)
-//                        {
-//                            backDelegate = StopHostClbk;
-//                        }
-//                        else
-//                        {
-//                            backDelegate = StopClientClbk;
-//                        }
-//                    }
-//                    else
-//                    {
-//                        if (conn.playerControllers[0].unetView.isClient)
-//                        {
-//                            backDelegate = StopHostClbk;
-//                        }
-//                        else
-//                        {
-//                            backDelegate = StopClientClbk;
-//                        }
-//                    }
-//                }
-//                else
-//                {
-//                    ChangeTo(mainMenuPanel);
-//                }
-//
-//                topPanel.ToggleVisibility(true);
-//                topPanel.isInGame = false;
-//            }
-//            else
-//            {
-//                ChangeTo(null);
-//
-//                Destroy(GameObject.Find("MainMenuUI(Clone)"));
-//
-//                //backDelegate = StopGameClbk;
-//                topPanel.isInGame = true;
-//                topPanel.ToggleVisibility(false);
-//            }
         }
-
-//        public void ChangeTo(RectTransform newPanel)
-//        {
-//            if (currentPanel != null)
-//            {
-//                currentPanel.gameObject.SetActive(false);
-//            }
-//
-//            if (newPanel != null)
-//            {
-//                newPanel.gameObject.SetActive(true);
-//            }
-//
-//            currentPanel = newPanel;
-//
-////            if (currentPanel != mainMenuPanel)
-////            {
-////                backButton.gameObject.SetActive(true);
-////            }
-////            else
-////            {
-////                backButton.gameObject.SetActive(false);
-////                SetServerInfo("Offline", "None");
-////                _isMatchmaking = false;
-////            }
-//        }
-
-        public void DisplayIsConnecting()
-        {
-            var _this = this;
-//            infoPanel.Display("Connecting...", "Cancel", () => { _this.backDelegate(); });
-        }
-
-//        public void SetServerInfo(string status, string host)
-//        {
-//            statusInfo.text = status;
-//            hostInfo.text = host;
-//        }
-
-
-//        public delegate void BackButtonDelegate();
-//        public BackButtonDelegate backDelegate;
-//        public void GoBackButton()
-//        {
-//            backDelegate();
-////			topPanel.isInGame = false;
-//        }
-
-        // ----------------- Server management
-		//add lobby player in room
-        public void AddLocalPlayer()
-        {
-            TryToAddPlayer();
-        }
-
-        public void RemovePlayer(LobbyPlayer player)
-        {
-            player.RemovePlayer();
-        }
-
-        public void SimpleBackClbk()
-        {
-//            ChangeTo(mainMenuPanel);
-        }
-                 
-        public void StopHostClbk()
-        {
-            if (_isMatchmaking)
-            {
-				matchMaker.DestroyMatch((NetworkID)_currentMatchID, 0, OnDestroyMatch);
-				_disconnectServer = true;
-            }
-            else
-            {
-                StopHost();
-            }
-
-            
-//            ChangeTo(mainMenuPanel);
-        }
-
-        public void StopClientClbk()
-        {
-            StopClient();
-
-            if (_isMatchmaking)
-            {
-                StopMatchMaker();
-            }
-
-//            ChangeTo(mainMenuPanel);
-        }
-
-        public void StopServerClbk()
-        {
-            StopServer();
-//            ChangeTo(mainMenuPanel);
-        }
-
-//        class KickMsg : MessageBase { }
-//        public void KickPlayer(NetworkConnection conn)
-//        {
-////            conn.Send(MsgKicked, new KickMsg());
-//        }
-
-        public void KickedMessageHandler(NetworkMessage netMsg)
-        {
-//            infoPanel.Display("Kicked by Server", "Close", null);
-            netMsg.conn.Disconnect();
-        }
-
-        //===================
-
-//        public override void OnStartHost()
-//        {
-//            base.OnStartHost();
-//
-////            ChangeTo(lobbyPanel);
-////            backDelegate = StopHostClbk;
-////            SetServerInfo("Hosting", networkAddress);
-//        }
 
 		public override void OnMatchCreate(bool success, string extendedInfo, MatchInfo matchInfo)
 		{
@@ -253,8 +80,6 @@ namespace Prototype.NetworkLobby
             int localPlayerCount = 0;
             foreach (PlayerController p in ClientScene.localPlayers)
                 localPlayerCount += (p == null || p.playerControllerId == -1) ? 0 : 1;
-
-//            addPlayerButton.SetActive(localPlayerCount < maxPlayersPerConnection && _playerNumber < maxPlayers);
         }
 
         // ----------------- Server callbacks ------------------
@@ -267,7 +92,6 @@ namespace Prototype.NetworkLobby
             GameObject obj = Instantiate(lobbyPlayerPrefab.gameObject) as GameObject;
 
             LobbyPlayer newPlayer = obj.GetComponent<LobbyPlayer>();
-            newPlayer.ToggleJoinButton(numPlayers + 1 >= minPlayers);
 
 
             for (int i = 0; i < lobbySlots.Length; ++i)
@@ -276,26 +100,11 @@ namespace Prototype.NetworkLobby
 
                 if (p != null)
                 {
-                    p.RpcUpdateRemoveButton();
-                    p.ToggleJoinButton(numPlayers + 1 >= minPlayers);
+//                    p.RpcUpdateRemoveButton();
                 }
             }
 
             return obj;
-        }
-
-        public override void OnLobbyServerPlayerRemoved(NetworkConnection conn, short playerControllerId)
-        {
-            for (int i = 0; i < lobbySlots.Length; ++i)
-            {
-                LobbyPlayer p = lobbySlots[i] as LobbyPlayer;
-
-                if (p != null)
-                {
-                    p.RpcUpdateRemoveButton();
-                    p.ToggleJoinButton(numPlayers + 1 >= minPlayers);
-                }
-            }
         }
 
         public override void OnLobbyServerDisconnect(NetworkConnection conn)
@@ -306,8 +115,7 @@ namespace Prototype.NetworkLobby
 
                 if (p != null)
                 {
-                    p.RpcUpdateRemoveButton();
-                    p.ToggleJoinButton(numPlayers >= minPlayers);
+//                    p.RpcUpdateRemoveButton();
                 }
             }
 
@@ -365,36 +173,15 @@ namespace Prototype.NetworkLobby
                 }
             }
 
-            for (int i = 0; i < lobbySlots.Length; ++i)
-            {
-                if (lobbySlots[i] != null)
-                {
-                    (lobbySlots[i] as LobbyPlayer).RpcUpdateCountdown(0);
-                }
-            }
+//            for (int i = 0; i < lobbySlots.Length; ++i)
+//            {
+//                if (lobbySlots[i] != null)
+//                {
+////                    (lobbySlots[i] as LobbyPlayer).RpcUpdateCountdown(0);
+//                }
+//            }
 
             ServerChangeScene(playScene);
-        }
-
-        // ----------------- Client callbacks ------------------
-
-        public override void OnClientConnect(NetworkConnection conn)
-        {
-            base.OnClientConnect(conn);
-
-        }
-
-
-        public override void OnClientDisconnect(NetworkConnection conn)
-        {
-            base.OnClientDisconnect(conn);
-//            ChangeTo(mainMenuPanel);
-        }
-
-        public override void OnClientError(NetworkConnection conn, int errorCode)
-        {
-//            ChangeTo(mainMenuPanel);
-//            infoPanel.Display("Cient error : " + (errorCode == 6 ? "timeout" : errorCode.ToString()), "Close", null);
         }
     }
 }
