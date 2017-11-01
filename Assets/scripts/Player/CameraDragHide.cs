2cb27690ee7c7aa49ed16eed497bfbe312b125ad
Penguinang <1157703273@qq.com>
2017-11-01 15:51:34 +0800

    试着加收回地图的功能

---
+++ Assets/scripts/Player/CameraDragHide.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDragHide : MonoBehaviour {
+
+	void OnMoseDown(){
+		Debug.Log ("drag connten on mouse down");
+	}
+}
